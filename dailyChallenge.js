d5abf8ed9c635d2ec985d39edbc09e388559f6ce
Aarushnautiyal <sandeepnautiyal1115@gmail.com>
2021-11-26 18:48:06 +0530

    date formatter

--- dailyChallenge.js
+++ dailyChallenge.js
@@ -1,10 +1,17 @@
-const list =(param)=>{
-    let array =[];
-    while (param > 0) {
-      array.push(param% 10);
-      param = Math.floor(param / 10);
-    }
-    return array;
+// const list =(param)=>{
+//     let array =[];
+//     while (param > 0) {
+//       array.push(param% 10);
+//       param = Math.floor(param / 10);
+//     }
+//     array.reverse()
+//     return array;
+// }
+
+// console.log(list(23456));
+
+const formatter = (theDate) =>{
+  return theDate.split("/").reverse().join("");
 }
 
-console.log(list(23456));
+console.log(formatter("12/03/144"))
